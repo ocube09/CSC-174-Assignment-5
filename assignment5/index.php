5f8f678e1cc4ec6c2cf4682535126e3931aac1e5
AlbertNissim <35936842+AlbertNissim@users.noreply.github.com>
2018-04-30 15:57:21 -0400

    Update index.php

--- assignment5/index.php
+++ assignment5/index.php
@@ -9,7 +9,7 @@
 <!-- Container which has a picture of pet owners with pets to visually present the  -->
 
 <div class="container-fluid bg-1 text-center home">
-  <img src="images/" class="img-responsive img-circle margin" style="display:inline" alt="Globe" width="250" height="250"> 
+  <img src="images/jungle.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Globe" width="250" height="250"> 
 </div>
 
 
@@ -28,7 +28,7 @@
   <div class="row">
 
     <div class="col-sm-4">
-      <img src="images/cat.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Cat" width="250" height="250">
+      <img src="images/kitten.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Cat" width="250" height="250">
       <p> The domestic cat (Felis silvestris catus or Felis catus)is a small, typically furry, carnivorous mammal. They are often called house cats when kept as indoor pets or simply cats when there is no need to distinguish them from other felids and felines. They are often valued by humans for companionship and for their ability to hunt vermin. There are more than seventy cat breeds recognized by various cat registries.Cats are similar in anatomy to the other felids, with a strong flexible body, quick reflexes, sharp retractable claws and teeth adapted to killing small prey.</p>
 
       <a href="cat.php" class="btn btn-default btn-lg">
@@ -38,7 +38,7 @@
     </div>
 
     <div class="col-sm-4"> 
-      <img src="images/parrot.jpg" class="img-responsive img-circle margin top " style="display:inline" alt="Parrot" width="250" height="250">
+      <img src="images/parrots.jpg" class="img-responsive img-circle margin top " style="display:inline" alt="Parrot" width="250" height="250">
       <p>Parrots may not make good pets for most people because of their natural wild instincts such as screaming and chewing. Although parrots can be very affectionate and cute when immature, they often become aggressive when mature (partly due to mishandling and poor training) and may bite, causing serious injury.For this reason, parrot rescue groups estimate that most parrots are surrendered and rehomed through at least five homes before reaching their permanent destinations or before dying prematurely from unintentional or intentional neglect and abuse.</p>
 
       <a href="parrot.php" class="btn btn-default btn-lg">
@@ -48,7 +48,7 @@
     </div>
 
     <div class="col-sm-4"> 
-      <img src="images/snake.jpg" class="img-responsive img-circle margin top " style="display:inline" alt="Snake" width="250" height="250">
+      <img src="images/cutesnake.jpg" class="img-responsive img-circle margin top " style="display:inline" alt="Snake" width="250" height="250">
       <p>In the Western world, some snakes (especially docile species such as the ball python and corn snake) are kept as pets. To meet this demand a captive breeding industry has developed. Snakes bred in captivity tend to make better pets and are considered preferable to wild caught specimens.Snakes can be very low maintenance pets, especially compared to more traditional species. They require minimal space, as most common species do not exceed 5 feet (1.5 m) in length. Pet snakes can be fed relatively infrequently, usually once every 5 to 14 days. Certain snakes have a lifespan of more than 40 years if given proper care.</p>
 
       <a href="snake.php" class="btn btn-default btn-lg">
